d863d93a77cdbe6be0b82e6d3c0b294c24f6e766
Grace Cumbi <146833513+Grace-cumbi@users.noreply.github.com>
2025-07-29 15:56:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Random Forest Classification Model for Assessing Land Suitable for Sorghum Production')
 
 st.write('Sorghum bicolor')
+df = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Data1.csv')
+df
