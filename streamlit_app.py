a869bb0123e2998cb1ffe969bf4b0fab742a450c
Grace Cumbi <146833513+Grace-cumbi@users.noreply.github.com>
2025-07-30 13:26:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('Random Forest Classification Model for Assessing Land Suitable for Sor
 
 st.write('Sorghum bicolor')
 df = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Data1.csv')
-df1 = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Refence table.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Refence.csv')
 df
 df1
