cde67dc6bec3995ada6ee44df544fb1861f591ea
Grace Cumbi <146833513+Grace-cumbi@users.noreply.github.com>
2025-08-05 11:07:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,21 +8,25 @@ with st.expander("Data"):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Data1.csv')
   df
-class = 1,2,3,4
+
+st.write('**X**')
+X = df.drop('x','y','Suitability', axis=1)
+X
+
+st.write('**y**')
+y = df.Suitability
+
 with st.sidebar:
   st.header('Input Features')
-  with st.expander('Temperature')
-    st.write ('**Temperature**')
-    class
-  Temperature = st.expander('Temperature', 1,2,3,4)
-  LULC = st.expander('LULC', 1,2,3,4)
-  Annual_Rainfall = st.expander('Rainfall', 1,2,3,4)
-  Slope = st.expander('Slope', 1,2,3,4)
-  Land_Surface_Temperature= st.expander('LST', 1,2,3,4)
-  Soil_PH = st.expander('PH', 1,2,3,4)
-  Soil_Carbon = st.expander('Carbon', 1,2,3,4)
-  Digital_Elevation_Model = st.expander('DEM', 1,2,3,4)
-  NDVI = st.expander('NDVI', 1,2,3,4)
-  Soil_Texture = st.expander('Texture', 1,2,3,4)
-  Soil_Nitrogen = st.expander('Nitrogen',1,2,3,4)
-  Cation_Exchange_Capacity = st.expander('CEC', 1,2,3,4)
+  Temperature = st.selectbox('Temperature', (1,2,3,4))
+  lulc = st.selectbox('LULC',(1,2,3,4))
+  Annual_Rainfall = st.selectbox('Rainfall', (1,2,3,4))
+  Slope = st.selectbox('Slope', (1,2,3,4))
+  Land_Surface_Temperature= st.selectbox('LST', (1,2,3,4))
+  Soil_PH = st.selectbox('PH', (1,2,3,4))
+  Soil_Carbon = st.selectbox('Carbon', (1,2,3,4))
+  Digital_Elevation_Model = st.selectbox('DEM', (1,2,3,4))
+  NDVI = st.selectbox('NDVI', (1,2,3,4))
+  Soil_Texture = st.selectbox('Texture', (1,2,3,4))
+  Soil_Nitrogen = st.selectbox('Nitrogen', (1,2,3,4))
+  Cation_Exchange_Capacity = st.selectbox('CEC', (1,2,3,4))
