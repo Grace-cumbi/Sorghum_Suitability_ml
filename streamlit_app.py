5a0933be762ed7577713a2cdc68eab65f5fc9e54
Grace Cumbi <146833513+Grace-cumbi@users.noreply.github.com>
2025-08-06 16:18:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.esemble import RandomForestClassifier
 
 st.title('Random Forest Classification Model for Assessing Land Suitable for Sorghum Production')
 st.image("https://github.com/Grace-cumbi/Sorghum_Suitability_ml/blob/master/Sorghum.png.jpg?raw=true")
@@ -9,24 +10,58 @@ with st.expander("Data"):
   df = pd.read_csv('https://raw.githubusercontent.com/Grace-cumbi/Sorghum_Suitability_ml/refs/heads/master/Data1.csv')
   df
 
-st.write('**X**')
-X = df.drop(columns = ['x','y','Suitability'])
-X
+  st.write('**X**')
+  X = df.drop(columns = ['x','y','Suitability'])
+  X
 
-st.write('**y**')
-y = df.Suitability
+  st.write('**y**')
+  y = df.Suitability
+  y
 
 with st.sidebar:
   st.header('Input Features')
-  Temperature = st.selectbox('Temperature', (1,2,3,4))
-  lulc = st.selectbox('LULC',(1,2,3,4))
-  Annual_Rainfall = st.selectbox('Rainfall', (1,2,3,4))
+  Temp = st.selectbox('Mean Annual Temperature', (1,2,3,4))
+  lulc = st.selectbox('Land Use Land Cover (LULC)',(1,2,3,4))
+  Rainfall = st.selectbox('Annual Rainfall', (1,2,3,4))
   Slope = st.selectbox('Slope', (1,2,3,4))
-  Land_Surface_Temperature= st.selectbox('LST', (1,2,3,4))
-  Soil_PH = st.selectbox('PH', (1,2,3,4))
-  Soil_Carbon = st.selectbox('Carbon', (1,2,3,4))
-  Digital_Elevation_Model = st.selectbox('DEM', (1,2,3,4))
-  NDVI = st.selectbox('NDVI', (1,2,3,4))
-  Soil_Texture = st.selectbox('Texture', (1,2,3,4))
-  Soil_Nitrogen = st.selectbox('Nitrogen', (1,2,3,4))
-  Cation_Exchange_Capacity = st.selectbox('CEC', (1,2,3,4))
+  LST = st.selectbox('Land Surface Temperature (LST)', (1,2,3,4))
+  PH = st.selectbox('Soil PH', (1,2,3,4))
+  Carbon = st.selectbox('Soil Carbon', (1,2,3,4))
+  DEM = st.selectbox('Digital Elevation Model (DEM)', (1,2,3,4))
+  NDVI = st.selectbox('Normalized Dense Vegetation Index (NDVI)', (1,2,3,4))
+  Texture = st.selectbox(' Soil Texture', (1,2,3,4))
+  Nitrogen = st.selectbox('Soil Nitrogen', (1,2,3,4))
+  CEC = st.selectbox('Cation Exchange Capacity (CEC)', (1,2,3,4))
+
+  data = {'Texture': Texture,
+          'PH': ph,
+          'CEC': CEC,
+          'Nitrogen': Nitrogen,
+          'Carbon': Carbon,
+          'Temp': Temp,
+          'Rainfall': Rainfall,
+          'Slope': Slope,
+          'DEM': DEM,
+          'NDVI': NDVI,
+          'LULC': LULC,
+          'LST': LST,}
+  input_df = pd.DataFrame(data, index[0])
+  input_suitability = pd.concat([input_df, X], axis=0)
+
+input_df
+
+clf = RandomForestClassifier()
+clf.fit(X,y)
+if st button("Predict"):
+  prediction = clf.predict(X)
+  if predicction >3.25:
+    st.write("Highly Suitable)
+  if prediction >2.5:
+    st.write("Moderately Suitable"
+  if prediction >1.75:
+    st.write("Marginally Suitable")
+  else :
+    st.write("Not Suitable")
+  
+
+  
